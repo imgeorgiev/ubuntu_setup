150dd28fff67871d5e966b5bfd9cf09f39b7e7a1
Ignat Georgiev <ignat.m.georgiev@gmail.com>
2019-09-08 17:24:21 +0100

    add xclip install

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 
 printf "\n## installing linux packages ##\n"
 
-packages="cifs-utils clipit calibre freecad handbrake redshift-gtk openvpn easy-rsa network-manager-openvpn-gnome kazam jstest-gtk meshlab pinta remmina synergy thunderbird nautilus vim xrdp vino gnome-tweak-tool pdftk terminator nmap python-catkin-tools mercurial"
+packages="cifs-utils clipit calibre freecad handbrake redshift-gtk openvpn easy-rsa network-manager-openvpn-gnome kazam jstest-gtk meshlab pinta remmina synergy thunderbird nautilus vim xrdp vino gnome-tweak-tool pdftk terminator nmap python-catkin-tools mercurial xclip"
 
 for pack in $packages; do
     apt-get install -y $pack
